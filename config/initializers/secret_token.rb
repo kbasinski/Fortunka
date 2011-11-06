174d3c41ea7711c8c4e9cfdea05baa689356d0bd
Krzysztof Basinski <k.basinski@artsalive.pl>
2011-11-06 14:03:47 +0100

    pierwszy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunka::Application.config.secret_token = '7b6c19d18667e88979bab46811d6e8fff9b07a818c04e97fb5632af8ae6e53c50005084785b588e62934614c383c1a362b893fbfcc0a0cd2390528bafa8ee437'
